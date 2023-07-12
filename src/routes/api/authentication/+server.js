382aa7d4575cf0310ae662b3e4a764b99420787a
Aryan Singh <32120119+singharyan@users.noreply.github.com>
2023-07-12 17:28:22 +0530

    u

--- src/routes/api/authentication/+server.js
+++ src/routes/api/authentication/+server.js
@@ -1,13 +1,13 @@
 import 'dotenv/config';
 import { json } from '@sveltejs/kit';
 
-const PASSWORD = process.env.PWD
+const PASSWORD = process.env.PSSWRD
 
 /** @type {import('./$types').RequestHandler} */
 export async function POST({ request }) {
-    const {pwd} = await request.json();
-    if(pwd == PASSWORD){
-        return json({auth: true, pwd: PASSWORD})
+    const { pwd } = await request.json();
+    if (pwd == PASSWORD) {
+        return json({ auth: true, pwd: PASSWORD })
     }
-    return json({auth: false, pwd: PASSWORD})
+    return json({ auth: false, pwd: PASSWORD })
 }
